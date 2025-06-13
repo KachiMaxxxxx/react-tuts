94b671b5a98463910758a3bfa5bb6b4d4cf19b60
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-06-13 01:54:02 +0100

    first mini project implemented

--- react-project-with-vite/src/main.jsx
+++ react-project-with-vite/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <main>
+    <img
+      src="/src/assets/react.svg"
+      alt="React Logo"
+      width="40px"
+    />
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first created in 2013</li>
+      <li>Was originally created by Jordan Walker</li>
+      <li>Has well over 200k stars on github</li>
+      <li>Is mantained by meta</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </main>
+);
