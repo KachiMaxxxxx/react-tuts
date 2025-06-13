120f41d1e376e2d1b0ce4d31599ba8558cf7bdcc
Kachi Maxxxxx <kachimax0101@gmail.com>
2025-06-13 12:11:50 +0100

    built first project with react

---
+++ react-project-with-vite/src/components/main.jsx
@@ -0,0 +1,16 @@
+export default function Main() {
+  return (
+    <>
+      <section className="main">
+        <h1 className="main__text">Fun facts about React</h1>
+        <ul className="main__list">
+          <li>Was first released in 2013</li>
+          <li>Was orignally created by Jordan Walke</li>
+          <li>Has well over 100K stars on GitHub</li>
+          <li>Is mantained by meta</li>
+          <li>Powers thousands of enterprises including mobile apps</li>
+        </ul>
+      </section>
+    </>
+  );
+}
---
+++ react-project-with-vite/src/components/navBar.jsx
@@ -0,0 +1,16 @@
+
+export default function NavBar() {
+  return (
+    <header className="header">
+      <nav className="navbar">
+        <img
+          src="/src/assets/react.svg"
+          alt="React Logo"
+          className="nav__img"
+        />
+
+        <span className="nav__text">ReactFacts</span>
+      </nav>
+    </header>
+  );
+}
